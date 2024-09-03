3354936e78786b03a64bb810be9929461324d7f8
ervanervan <ervanervan040@gmail.com>
2024-09-03 20:19:46 +0700

    add: header style

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <section>Header</section>;
 };
 
 export default Header;
