9695de0226735bc5484a5bf04a04dde9aeb4e164
ervanervan <ervanervan040@gmail.com>
2024-09-04 20:10:31 +0700

    update: appwrap component

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { CgWebsite } from "react-icons/cg";
-import { FaWebAwesome } from "react-icons/fa6";
 
 const SocialMedia = () => (
   <div className="app__social">
