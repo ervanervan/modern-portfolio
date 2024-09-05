bedc970c05dbf68af45c63065709d53c2b888a9f
ervanervan <ervanervan040@gmail.com>
2024-09-05 22:34:59 +0700

    update: work component

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -143,7 +143,7 @@ const Work = () => {
               <motion.div
                 whileHover={{ opacity: 1 }}
                 initial={{ opacity: 0 }} // Ensure opacity is 0 initially
-                transition={{ duration: 0.25, ease: "easeInOut" }} // Remove staggerChildren for smoother interaction
+                transition={{ duration: 0.25, ease: "easeInOut" }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
